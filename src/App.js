4512999b387431c245263298019b0fc4a9b87f9a
MoisesNer <StarCommand@Moisess-iMac.local>
2022-03-07 20:32:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion'
+import { useRef, useEffect, useState} from "react"
+import images from './Pictures/images'
 
 function App() {
+  // console.log(images);
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    // console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <motion.h1 animate={{ x:-100 }}>HELLO WORLD</motion.h1> */}
+      <motion.div className='carousel' ref={carousel} whileTap={{cursor: 'grabbing'}}>
+        <motion.div className='inner-carousel' drag='x' dragConstraints={{ right: 0, left: -width}}>
+          {images.map(image => {
+            return(
+              <motion.div className='item'>
+                <img src={image}  key={image} />
+              </motion.div>
+            )
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
